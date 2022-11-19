8362498ddbc0f6ef340ae27df87b3dd5f9e6f511
aditya-bhat646 <adityabhat646@gmail.com>
2022-11-19 12:12:53 +0530

    Shifted Stateful logic to Form-Context

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,106 +1,35 @@
-import React, {
-  useState,
-  useEffect,
-  useReducer,
-  useContext,
-} from 'react';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
-import AuthContext from '../store/Auth-Context';
 import Input from '../UI/Input/Input';
-
-const defaultFormState = {
-  email: '',
-  emailIsValid: undefined,
-  password: '',
-  passwordIsValid: undefined,
-};
-
-const formInputReducer = (state, action) => {
-  if (action.type === 'EMAIL_INPUT') {
-    return {
-      ...state,
-      email: action.payload,
-      emailIsValid: action.payload.includes('@'),
-    };
-  }
-
-  if (action.type === 'PASSWORD_INPUT') {
-    return {
-      ...state,
-      password: action.payload,
-      passwordIsValid: action.payload.trim().length > 6,
-    };
-  }
-
-  return defaultFormState;
-};
+import FormContext from '../store/Form-Validity';
 
 function Login() {
-  const [formIsValid, setFormIsValid] = useState(false);
-
-  const authContext = useContext(AuthContext);
-
-  const [formState, dispatchFormAction] = useReducer(
-    formInputReducer,
-    defaultFormState,
-  );
-
-  useEffect(() => {
-    const timerIdentifier = setTimeout(() => {
-      if (formState.email.includes('@')
-      && formState.password.length > 6) {
-        setFormIsValid(true);
-      } else {
-        setFormIsValid(false);
-      }
-    }, 500);
-
-    return () => clearTimeout(timerIdentifier);
-  }, [formState.emailIsValid, formState.passwordIsValid]);
-
-  const emailChangeHandler = (event) => {
-    dispatchFormAction({
-      type: 'EMAIL_INPUT',
-      payload: event.target.value,
-    });
-  };
-
-  const passwordChangeHandler = (event) => {
-    dispatchFormAction({
-      type: 'PASSWORD_INPUT',
-      payload: event.target.value,
-    });
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    authContext.onLogin(formState.email, formState.password);
-  };
+  const formContext = useContext(FormContext);
 
   return (
     <Card className={classes.login}>
-      <form onSubmit={submitHandler}>
+      <form onSubmit={formContext.submitHandler}>
         <Input
           label="Email"
           id="email"
           type="email"
-          onChange={emailChangeHandler}
-          value={formState.email}
-          isValid={formState.emailIsValid}
+          onChange={formContext.emailChangeHandler}
+          value={formContext.formState.email}
+          isValid={formContext.formState.emailIsValid}
         />
         <Input
           label="Password"
           id="password"
           type="password"
-          onChange={passwordChangeHandler}
-          value={formState.password}
-          isValid={formState.passwordIsValid}
+          onChange={formContext.passwordChangeHandler}
+          value={formContext.formState.password}
+          isValid={formContext.formState.passwordIsValid}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} disabled={!formContext.formIsValid}>
             Login
           </Button>
         </div>
