1d0572c6130a6eda68321d039c59339b1041c117
aditya-bhat646 <adityabhat646@gmail.com>
2022-11-19 12:11:18 +0530

    Wrapped FormContextProvider around <Logic />

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AuthContext from './components/store/Auth-Context';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import { FormContextProvider } from './components/store/Form-Validity';
 
 function App() {
   const authContext = useContext(AuthContext);
@@ -13,7 +14,12 @@ function App() {
     <>
       <MainHeader />
       <main>
-        {!authContext.isLoggedIn && <Login />}
+        {!authContext.isLoggedIn
+        && (
+          <FormContextProvider>
+            <Login />
+          </FormContextProvider>
+        )}
         {authContext.isLoggedIn && <Home />}
       </main>
     </>
